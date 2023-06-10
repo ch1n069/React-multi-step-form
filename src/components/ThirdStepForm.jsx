3e23456515c3e5a1aaec86047a96ced6238b61f0
Bruno Alpachino <brunolalpachino@gmail.com>
2023-06-10 07:47:40 +0300

    ft == created the second and third step from

---
+++ src/components/ThirdStepForm.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Button, TextField } from "@mui/material";
+const ThirdStepForm = () => {
+  return (
+    <div style={{}}>
+      <div>
+        <TextField
+          margin="normal"
+          label="First Name"
+          variant="outlined"
+          color="secondary"
+        />
+      </div>
+
+      <div>
+        <TextField
+          margin="normal"
+          label="Second Name"
+          variant="outlined"
+          color="secondary"
+        />
+      </div>
+      <div>
+        <TextField
+          margin="normal"
+          label="Contact info"
+          variant="outlined"
+          color="secondary"
+        />
+      </div>
+      <Button variant="contained" color="secondary">
+        Next
+      </Button>
+    </div>
+  );
+};
+
+export default ThirdStepForm;
